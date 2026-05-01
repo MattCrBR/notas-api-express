b3047fc7b853d3edfee8720602c21a5bc27ff494
MattCrBR <matheuscravo62@gmail.com>
2026-05-01 12:55:10 -0300

    feat: cria a api RESTful server.js com Express

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+const FILE = 'data.json';
+
+// permite receber JSON
+app.use(bodyParser.json());
+
+// libera acesso externo (CORS)
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// funcao para ler arquvio
+function readNotes(){
+    try {
+        const data = fs.readFileSync(FILE);
+        return JSON.parse(data);    
+    } catch {
+        return [];
+    }
+}
+
+// funcao para salvar arquivo
+function saveNotes(notes){
+    fs.writeFileSync(FILE, JSON.stringify(notes, null, 2));
+}
+
+// GET - lista notas
+app.get('/api/notes', (req, res) => {
+    const notes = readNotes();
+    res.json(notes);
+});
+
+// POST - cria nota
+app.post('/api/notes', (req, res) => {
+    const notes = readNotes();
+    const novaNota = {
+        id: Date.now().toString(),
+        titulo: req.body.titulo,
+        texto: req.body.texto
+    };
+    notes.push(novaNota);
+    saveNotes(notes);
+    res.json(novaNota);
+});
+
+// PUT - edita nota
+app.put('/api/notes/:id', (req, res) => {
+    const notes = readNotes();
+    const index = notes.findIndex(n=>n.id==req.params.id);
+    if(index>=0){
+        notes[index].titulo = req.body.titulo;
+        notes[index].texto = req.body.texto;
+        saveNotes(notes);
+        res.json(notes[index]);
+    } else {
+        res.status(404).json({ error: 'Nota não encontrada' });
+    }
+});
+
+// DELETE - exclui nota
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = readNotes();
+    const novasNotas = notes.filter(n=>n.id!==req.params.id);
+    saveNotes(novasNotas);
+    res.json({ mensagem: 'Nota removida' });
+});
+
+// inicia servidor
+app.listen(PORT, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
